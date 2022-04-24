e8a7a2f6ec33936084c8c1a07d79a6be21547d6b
JDMR10 <josedavid@vamos.ml>
2022-04-24 14:06:39 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -5,6 +5,7 @@
 
 function show(output){
 document.getElementById('output').innerHTML += '<p>'+output+'</p>';
+document.getElementById('moneda2').innerHTML += '<p>'+output+'</p>';  
 }
 
 show(location.href);
